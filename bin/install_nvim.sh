05c9efec220d13e13a442dfb3338ee9f25a542f0
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2025-05-03 15:07:44 -0500

    Fix errors on install for two scripts

--- bin/install_nvim.sh
+++ bin/install_nvim.sh
@@ -3,8 +3,8 @@ bin_dir=$script_dir/../nvim/bin
 target_dir=${HOME}/.local/bin
 for file in $bin_dir/*.sh $bin_dir/nvim $bin_dir/nvim_shell $bin_dir/mkchad; do
   filename=$(basename $file)
-  if [ -e "$target_dir/$file" ]; then
-    rm -v $target_dir/$file;
+  if [ -e "$target_dir/$filename" ]; then
+    rm -v $target_dir/$filename;
   fi;
-  install -Dv -m755 $file $target_dir
+  install -Dv -m755 $file $target_dir/$filename
 done;
--- bin/install_tools.sh
+++ bin/install_tools.sh
@@ -3,8 +3,8 @@ bin_dir=$script_dir/../container_tools
 target_dir=${HOME}/.local/bin
 for file in $bin_dir/*.sh; do
   filename=$(basename $file)
-  if [ -e "$target_dir/$file" ]; then
-    rm -v $target_dir/$file;
+  if [ -e "$target_dir/$filename" ]; then
+    rm -v $target_dir/$filename;
   fi;
-  install -Dv -m755 $file $target_dir
+  install -Dv -m755 $file $target_dir/$filename
 done;
