2d9da74953dc7a67ebd70989df02411a2fb0ba88
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2026-05-25 13:14:58 -0500

    Adjust install scripts adjust permissions

--- bin/install_nvim.sh
+++ bin/install_nvim.sh
@@ -1,7 +1,7 @@
 script_dir=$(dirname $(realpath $0))
 bin_dir=$script_dir/../nvim/bin
 target_dir=${HOME}/.local/bin
-for file in $bin_dir/*.sh $bin_dir/nvim $bin_dir/nvim_shell $bin_dir/mkchad; do
+for file in $bin_dir/*.sh $bin_dir/nvim $bin_dir/nvim_shell $bin_dir/nvim_clear_data $bin_dir/mkchad; do
   filename=$(basename $file)
   if [ -e "$target_dir/$filename" ]; then
     rm -v $target_dir/$filename;
