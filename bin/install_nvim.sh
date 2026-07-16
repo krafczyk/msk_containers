01a7355b43f64b5ee39ed59905c7e4c2c6041012
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2026-07-16 14:35:47 -0500

    feat(nvim): automate container installation

--- bin/install_nvim.sh
+++ bin/install_nvim.sh
@@ -1,10 +1,19 @@
-script_dir=$(dirname $(realpath $0))
-bin_dir=$script_dir/../nvim/bin
-target_dir=${HOME}/.local/bin
-for file in $bin_dir/*.sh $bin_dir/nvim $bin_dir/nvim_shell $bin_dir/nvim_clear_data $bin_dir/mkchad; do
-  filename=$(basename $file)
-  if [ -e "$target_dir/$filename" ]; then
-    rm -v $target_dir/$filename;
-  fi;
-  install -Dv -m755 $file $target_dir/$filename
-done;
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+script_dir=$(dirname "$(realpath "$0")")
+bin_dir="$script_dir/../nvim/bin"
+target_dir="$HOME/.local/bin"
+files=(
+  "$bin_dir"/*.sh
+  "$bin_dir/nvim"
+  "$bin_dir/nvim_shell"
+  "$bin_dir/nvim_clear_data"
+  "$bin_dir/mkchad"
+  "$bin_dir/install_nvim_container"
+)
+
+for file in "${files[@]}"; do
+  install -Dv -m755 "$file" "$target_dir/${file##*/}"
+done
